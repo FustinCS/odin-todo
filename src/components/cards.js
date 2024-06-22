b82f0a2b9aee62de1864336d68bcd314ea4817b2
Justin J <132813834+FustinCS@users.noreply.github.com>
2024-06-22 04:00:47 -0700

    Add Todo item Card Component

---
+++ src/components/cards.js
@@ -0,0 +1,23 @@
+import { TodoItem } from "../objects/todo";
+
+function itemCardComponent(todoItem) {
+    const card = document.createElement('div');
+    const title = document.createElement('h2');
+    const description = document.createElement('p');
+    const dueDate = document.createElement('p');
+    const priority = document.createElement('p');
+
+    title.textContent = todoItem.title;
+    description.textContent = todoItem.description;
+    dueDate.textContent = todoItem.dueDate;
+    priority.textContent = todoItem.dueDate;
+
+    card.appendChild(title);
+    card.appendChild(description);
+    card.appendChild(dueDate);
+    card.appendChild(priority);
+
+    return card;
+}
+
+export { itemCardComponent };
